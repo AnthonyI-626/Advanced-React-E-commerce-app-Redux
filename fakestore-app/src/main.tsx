e5518cb79943c6a42a5486c4896a669ad6f38019
Anthony Irizarry <tonyirizarry69@gmail.com>
2025-12-16 19:37:01 -0700

    typo fix

--- fakestore-app/src/main.tsx
+++ fakestore-app/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {Provider} from 'react-redux';
-import {store} from './store/store.ts';
+import {store} from './store/store';
 
 const queryClient = new QueryClient();
 
